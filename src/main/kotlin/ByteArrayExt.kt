3a490d97c2ab9ce2836e70a26eda4a41851939b8
Yuiki <contact@yuiki.jp>
2018-03-12 12:49:46 +0900

    impl Base58 and Base58Check

--- src/main/kotlin/ByteArrayExt.kt
+++ src/main/kotlin/ByteArrayExt.kt
@@ -1,5 +1,37 @@
+import java.math.BigInteger
+
+const val MAP_BASE58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+val FIFTY_EIGHT: BigInteger by lazy { BigInteger.valueOf(58L) }
+
 fun ByteArray.toHex() = StringBuilder().apply {
     for (i in this@toHex) {
         append(String.format("%02X", i))
     }
 }.toString()
+
+fun ByteArray.toBase58Check(): String {
+    val hashed = Hash.applySHA256(this, 2)
+    val extended = ByteArray(this.size + 4)
+    System.arraycopy(this, 0, extended, 0, this.size)
+    System.arraycopy(hashed, 0, extended, this.size, 4)
+    return extended.toBase58()
+}
+
+fun ByteArray.toBase58(): String {
+    var leadingZeroBytes = 0
+    while (leadingZeroBytes < this.size && this[leadingZeroBytes] == 0.toByte()) {
+        leadingZeroBytes++
+    }
+
+    var input = BigInteger(this)
+    val output = StringBuilder()
+    while (input > BigInteger.ZERO) {
+        val results = input.divideAndRemainder(FIFTY_EIGHT)
+        output.append(MAP_BASE58[results[1].toInt()])
+        input = results[0]
+    }
+    for (i in leadingZeroBytes - 1 downTo 0) {
+        output.append(MAP_BASE58[0])
+    }
+    return output.reverse().toString()
+}
--- src/main/kotlin/Hash.kt
+++ src/main/kotlin/Hash.kt
@@ -1,23 +1,33 @@
+import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.security.MessageDigest
+import java.security.Security
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
-const val HMAC_SHA512 = "HmacSHA512"
-const val SHA256 = "SHA-256"
-const val PROVIDER = "BC"
+class Hash {
+    companion object {
+        private const val HMAC_SHA512 = "HmacSHA512"
+        private const val SHA256 = "SHA-256"
+        private const val PROVIDER = "BC"
 
-fun applyHmacSHA512(data: ByteArray, key: ByteArray): ByteArray {
-    val keySpec = SecretKeySpec(key, HMAC_SHA512)
-    val mac = Mac.getInstance(HMAC_SHA512, PROVIDER).apply { init(keySpec) }
-    return mac.doFinal(data)
-}
+        init {
+            Security.addProvider(BouncyCastleProvider())
+        }
 
-fun applySHA256(data: ByteArray, round: Int): ByteArray {
-    val md = MessageDigest.getInstance(SHA256)
-    var digested = data
-    for (i in 0 until round) {
-        md.update(digested)
-        digested = md.digest()
+        fun applyHmacSHA512(data: ByteArray, key: ByteArray): ByteArray {
+            val keySpec = SecretKeySpec(key, HMAC_SHA512)
+            val mac = Mac.getInstance(HMAC_SHA512, PROVIDER).apply { init(keySpec) }
+            return mac.doFinal(data)
+        }
+
+        fun applySHA256(data: ByteArray, round: Int): ByteArray {
+            val md = MessageDigest.getInstance(SHA256)
+            var digested = data
+            for (i in 0 until round) {
+                md.update(digested)
+                digested = md.digest()
+            }
+            return digested
+        }
     }
-    return digested
 }
