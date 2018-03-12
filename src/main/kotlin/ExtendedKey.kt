82900d1af380bb9e4f5434744594a35b616fd5fc
Yuiki <contact@yuiki.jp>
2018-03-12 13:10:06 +0900

    impl priv key

--- src/main/kotlin/ExtendedKey.kt
+++ src/main/kotlin/ExtendedKey.kt
@@ -1,16 +1,54 @@
+import Hash.Companion.applyHmacSHA512
+import java.io.ByteArrayOutputStream
 import java.util.*
 
-class ExtendedKey(seed: ByteArray) {
+class ExtendedKey(testnet: Boolean, seed: ByteArray) {
     val chainCode: ByteArray
+    private val depth: Int
+    private val parentFingerprint: Int
+    private val childNumber: Int
+    private val keyPriv: ByteArray
+
+    private val verPub = if (testnet) TPUB else XPUB
+    private val verPriv = if (testnet) TPRIV else XPRIV
 
     companion object {
-        val key = "Bitcoin seed".toByteArray()
+        val KEY_HASH = "Bitcoin seed".toByteArray()
+
+        val XPUB = byteArrayOf(0x04, 0x88.toByte(), 0xB2.toByte(), 0x1E)
+        val XPRIV = byteArrayOf(0x04, 0x88.toByte(), 0xAD.toByte(), 0xE4.toByte())
+        val TPUB = byteArrayOf(0x04, 0x35, 0x87.toByte(), 0xCF.toByte())
+        val TPRIV = byteArrayOf(0x04, 0x35, 0x83.toByte(), 0x94.toByte())
     }
 
     init {
-        val hashedSeed = applyHmacSHA512(seed, key)
+        val hashedSeed = applyHmacSHA512(seed, KEY_HASH)
         val hashedLeft = Arrays.copyOfRange(hashedSeed, 0, 32)
         val hashedRight = Arrays.copyOfRange(hashedSeed, 32, 64)
         chainCode = hashedRight
+
+        depth = 0
+        parentFingerprint = 0
+        childNumber = 0
+        keyPriv = hashedLeft
     }
+
+    val privKey: String by lazy { serialize(true, keyPriv) }
+
+    private fun serialize(isPriv: Boolean, key: ByteArray): String =
+            ByteArrayOutputStream().apply {
+                write(if (isPriv) verPriv else verPub)
+                write(depth and 0xFF)
+                write(parentFingerprint ushr 24 and 0xFF)
+                write(parentFingerprint ushr 16 and 0xFF)
+                write(parentFingerprint ushr 8 and 0xFF)
+                write(parentFingerprint and 0xFF)
+                write(childNumber ushr 24 and 0xFF)
+                write(childNumber ushr 16 and 0xFF)
+                write(childNumber ushr 8 and 0xFF)
+                write(childNumber and 0xFF)
+                write(chainCode)
+                if (isPriv) write(0x00)
+                write(key)
+            }.toByteArray().toBase58Check()
 }
--- src/test/kotlin/ExtendedKeyTest.kt
+++ src/test/kotlin/ExtendedKeyTest.kt
@@ -1,24 +1,17 @@
-import org.bouncycastle.jce.provider.BouncyCastleProvider
+import Hash.Companion.applySHA256
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import java.security.Security
 
 class ExtendedKeyTest {
-    companion object {
-        @JvmStatic
-        @BeforeAll
-        fun init() {
-            Security.addProvider(BouncyCastleProvider())
-        }
-    }
-
     @Test
     fun testExtendedKey() {
         val passPhrase = "check this one *&##X87 check this 2 this passphrase 593 is tough to crack&@!!"
         val round = 50000
         val hashedPassPhrase = applySHA256(passPhrase.toByteArray(), round)
-        val chainCode = ExtendedKey(hashedPassPhrase).chainCode
+        val key = ExtendedKey(false, hashedPassPhrase)
+        val chainCode = key.chainCode
         Assertions.assertTrue(chainCode.toHex().toLowerCase() == "a24a2cae513d6c70c4076d685c56945b565810ee70bdb96198a2b60b83e1e7ba".toLowerCase())
+        val privKey = key.privKey
+        Assertions.assertTrue(privKey == "xprv9s21ZrQH143K3g56LSYgKiKx4LD4GJh27DUXRqShDpn2cAD4EAPbVn5T9BqqcGswHzcrztzJEJMHpS7wstai53cS6esPPkRGjp4voMhXrTP")
     }
 }
